29c0a42d62f686e312b336a73346328cab490327
Marcus <Marcus@DESKTOP-SBSA44E>
2024-06-23 22:30:57 +0100

    account settings

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "./../../ui/SpinnerMini";
 
 function LoginForm() {
-    const [email, setEmail] = useState("marcus@test.com");
-    const [password, setPassword] = useState("911961");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const { login, isLoading } = useLogin();
 
     function handleSubmit(e) {
